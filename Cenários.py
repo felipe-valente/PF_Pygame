dbe443c055e944cfb32b0273630d19b9cf82a950
FelipeValente <38335857+felipe-valente@users.noreply.github.com>
2019-05-07 16:44:12 -0300

    Add files via upload

---
+++ Cenários.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 16:41:50 2019
+
+@author: felip
+"""
+
+print("cenários")
---
+++ Sprites.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 16:41:19 2019
+
+@author: felip
+"""
+print("sprites")
