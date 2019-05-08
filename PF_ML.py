47296503675b05449ec48b43165cd48349565ebd
Felipe Valente <felipevalente2901@gmail.com>
2019-05-08 15:28:26 -0300

    Esqueleto do código

--- PF_ML.py
+++ PF_ML.py
@@ -1,8 +1,35 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue May  7 16:26:59 2019
+import pygame
+import random
 
-@author: felip
-"""
 
-print("Hello world!")
+WIDTH = 360
+HEIGHT = 480
+FPS = 100
+
+preto = (0,0,0)
+branco = (255,255,255)
+azul = (0,0,255)
+vermelho = (255,0,0)
+verde = (0,255,0)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Ship lander")
+clock = pygame.time.Clock()
+
+#Game loop
+rodando = True
+while rodando:
+	#deixa o loop rodadando na velocidade certa
+	clock.tick(FPS)
+	#processos de inputs/eventos
+	for event in pygame.event.get():
+		#fecha o jogo no "X"
+		if event.type == pygame.QUIT:
+			rodando = False
+
+	screen.fill(preto)
+	pygame.display.flip()
+
+pygame.quit()
